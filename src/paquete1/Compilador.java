bd113849192ccbf6c2729a6ee25f6c7ceece057d
Ariel Bertino <49039560+arielbertino@users.noreply.github.com>
2020-09-19 19:43:33 -0300

    clase matrzTEstados actualizada

--- src/paquete1/Compilador.java
+++ src/paquete1/Compilador.java
@@ -1,9 +1,10 @@
+// Clases central desde donde se invoca todo
 package paquete1;
 
 public class Compilador {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
